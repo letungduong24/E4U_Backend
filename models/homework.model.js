99dd18a645e7bc125931869099d0727fec81d8c9
ldngduong <letungduong1624@gmail.com>
2025-10-27 16:47:05 +0700

    add title to hw

--- models/homework.model.js
+++ models/homework.model.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const homeworkSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Document title is required'],
+    trim: true,
+    maxlength: [200, 'Title cannot be more than 200 characters']
+  },
   classId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Class',
